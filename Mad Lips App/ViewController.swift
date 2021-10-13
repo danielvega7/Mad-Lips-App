7a408aeda66816ca59a46fa2bb05ffe77592650f
Daniel Vega <daniel.vega23@d155.org>
2021-10-13 08:10:02 -0500

    on stretch 3

--- Mad Lips App/ViewController.swift
+++ Mad Lips App/ViewController.swift
@@ -7,25 +7,84 @@
 
 import UIKit
 
+class StaticVars {
+    
+    static var themes = ["food", "soccer", "compututer science"]
+    
+    static var foodsentence = "The _adjective_ _noun_ very much _verb_ my hunger"
+    static var soccersentence = "The _adjective_ soccer player _verb_ because his _noun_ told him to"
+    static var compscisentence = "_verb_ the computer lets _adjective_ _noun_ improve their skills"
+    
+    
+    
+}
+
+
+
 class ViewController: UIViewController {
     @IBOutlet weak var vc1TextFieldOutlet: UITextField!
+    @IBOutlet weak var vc1TextFieldAdjectiveOutlet: UITextField!
+    @IBOutlet weak var vc1TextFieldVerbOutlet: UITextField!
+    @IBOutlet weak var themeLabel: UILabel!
+    @IBOutlet weak var textViewOutlet: UITextView!
     
+    
+    var randomNum = 0
+    var theme = ""
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      
+        randomNum = Int.random(in: 0...2)
+        theme = StaticVars.themes[randomNum]
+        
+        themeLabel.text = "Theme: \(theme)"
+        
+        if theme == "food" {
+            textViewOutlet.text = StaticVars.foodsentence
+        }
+        else if theme == "soccer" {
+            textViewOutlet.text = StaticVars.soccersentence
+        }
+        else{
+            textViewOutlet.text = StaticVars.compscisentence
+        }
+        
     }
    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
         let nvc = segue.destination as! ViewController2
             nvc.word = vc1TextFieldOutlet.text!
-        
+            nvc.adjective = vc1TextFieldAdjectiveOutlet.text!
+            nvc.verb = vc1TextFieldVerbOutlet.text!
+            nvc.vc2theme = theme
+            
     
     
 
 }
 
     @IBAction func goToVC2Action(_ sender: UIButton) {
+        
+        if vc1TextFieldOutlet.text == "" || vc1TextFieldAdjectiveOutlet.text == "" || vc1TextFieldVerbOutlet.text == ""{
+            
+            let alert = UIAlertController(title: "Textfield empty", message: "Put something in the text fields or else...", preferredStyle: .alert)
+
+            alert.addAction(UIAlertAction(title: "Yes, will do", style: .default, handler: nil))
+                
+          
+            //alert.addAction(UIAlertAction(title: "No", style: .cancel, handler: nil))
+
+            self.present(alert, animated: true)
+            
+        }
+        else{
         performSegue(withIdentifier: "jamal", sender: nil)
+        }
     }
+
+    
+    
+    
+    
 }
--- Mad Lips App/ViewController2.swift
+++ Mad Lips App/ViewController2.swift
@@ -12,16 +12,43 @@ class ViewController2: UIViewController {
     @IBOutlet weak var vc2TextViewOutlet: UITextView!
     
     var word = ""
+    var verb = ""
+    var adjective = ""
+    var vc2theme = ""
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        if vc2theme == "food" {
+            vc2TextViewOutlet.text = StaticVars.foodsentence
+        }
+        else if vc2theme == "soccer" {
+            vc2TextViewOutlet.text = StaticVars.soccersentence
+        }
+        else{
+            vc2TextViewOutlet.text = StaticVars.compscisentence
+        }
+        
+        
+      
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
-        vc2TextViewOutlet.text = "My uncle wants to go to the \(word)"
+        
+        if vc2theme == "food" {
+            //change it 
+            vc2TextViewOutlet.text = StaticVars.foodsentence
+        }
+        else if vc2theme == "soccer" {
+            vc2TextViewOutlet.text = StaticVars.soccersentence
+        }
+        else{
+            vc2TextViewOutlet.text = StaticVars.compscisentence
+        }
+        
+       
     }
 
 
